cc5a93bbec46714561efccebebbfd355a783d173
ryu19j <ryu.19j.5@gmail.com>
2017-01-02 23:37:43 +0900

    JAX-RSサンプル作成。

---
+++ jaxrs-sample/src/main/java/sample/App.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("app")
+public class App {
+
+	@Path("hello")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello(@QueryParam("name") String name) {
+		return "Hello, " + name + "!!";
+	}
+}
---
+++ jaxrs-sample/src/test/java/sample/AppTest.java
@@ -0,0 +1,23 @@
+package sample;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+public class AppTest extends JerseyTest {
+
+	@Test
+	public void test() throws Exception {
+		String hello = target("app/hello").queryParam("name", "Ryu").request().get(String.class);
+		assertThat(hello, is("Hello, Ryu!!"));
+	}
+
+	protected Application configure() {
+		return new ResourceConfig(App.class);
+	}
+}
